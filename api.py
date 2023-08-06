8643d4fd7450396f340ef04c079fffaf794eb1cf
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-08-06 00:48:35 -0300

    Incluindo BD para criação de user.

--- api.py
+++ api.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import jwt
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisissecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/José Filho/Desktop/Udemy/RESTFul API Flask SQLalquemy/todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 
 db = SQLAlchemy(app)
 
@@ -16,7 +16,7 @@ class User(db.Model):
     public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(50))
     password = db.Column(db.String(80))
-    admin = db.Column(db.Boolean)
+    admin = db.Column(db.Boolean, default=False)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,21 +24,49 @@ class Todo(db.Model):
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer)
 
+
 @app.route('/user', methods=['GET'])
 def get_all_users():
-    return ''
+    users = User.query.all()
 
-@app.route('/user/<user_id>', methods=['GET'])
-def get_one_user():
-    return ''
+    output = []
 
-@app.route('/user/<user_id>', methods=['POST'])
+    for user in users:
+        user_data = {}
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        output.append(user_data)
+
+    return jsonify({'users': output})
+
+@app.route('/user/<public_id>', methods=['GET'])
+def get_one_user(public_id):
+
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message': 'No  user found!'})
+
+    user_data = {}
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+
+    return jsonify({'user': user_data})
+
+@app.route('/user', methods=['POST'])
 def create_user():
     data = request.get_json()
 
     hashed_password = generate_password_hash(data['password'], method='sha256')
 
-    return ''
+    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'nessage': 'New user created!'})
 
 @app.route('/user/<user_id>', methods=['PUT'])
 def promote_user():
