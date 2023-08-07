4f63bd93b50baf68c930cff13ef7db7be46578d2
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-08-07 19:51:54 -0300

    POST fazendo criação de login de user com sucesso!

--- api.py
+++ api.py
@@ -1,29 +1,36 @@
+import os
+
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 import uuid
-from werkzeug.security import generate_password_hash, check_password_hash
-import jwt
+from passlib.hash import sha256_crypt  # Importe a função sha256_crypt do passlib
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+
+file_path = os.path.abspath(os.getcwd())+"/database.db" # permite criar o BD na raiz
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'thisissecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
 
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(50))
     password = db.Column(db.String(80))
-    admin = db.Column(db.Boolean, default=False)
+    admin = db.Column(db.Boolean)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(50))
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer)
-
 
 @app.route('/user', methods=['GET'])
 def get_all_users():
@@ -47,7 +54,7 @@ def get_one_user(public_id):
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user:
-        return jsonify({'message': 'No  user found!'})
+        return jsonify({'message': 'No  user found! | Nenhum usuário encontrado!'})
 
     user_data = {}
     user_data['public_id'] = user.public_id
@@ -61,22 +68,34 @@ def get_one_user(public_id):
 def create_user():
     data = request.get_json()
 
-    hashed_password = generate_password_hash(data['password'], method='sha256')
+    hashed_password = sha256_crypt.hash(data['password'])  # Use sha256_crypt para gerar o hash da senha
 
     new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({'nessage': 'New user created!'})
 
-@app.route('/user/<user_id>', methods=['PUT'])
-def promote_user():
-    return ''
+    # Gerar um token JWT para o novo usuário criado
+    access_token = create_access_token(identity=new_user.public_id)
 
-@app.route('/user/<user_id>', methods=['DELETE'])
+    return jsonify({'message': 'New user created! | Novo usuário criado!', 'access_token': access_token}), 201
+@app.route('/user/<public_id>', methods=['PUT'])
+def promote_user(public_id):
+
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({'message': 'No  user found! | Nenhum usuário encontrado!'})
+
+    user.admin = True
+    db.session.commit()
+    return jsonify({'message': 'The user has been promoted! | O usuário foi promovido!'})
+
+@app.route('/user/<public_id>', methods=['DELETE'])
 def delete_user():
     return ''
 
-
-
 if __name__ == '__main__':
+    with app.app_context():
+        # Criar as tabelas no banco de dados
+        db.create_all()
     app.run(debug=True)
