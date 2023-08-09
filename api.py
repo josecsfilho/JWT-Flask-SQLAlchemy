bef2b89e64d6eeba98ab0a8d30bd6eb85fb64dcd
José Filho <100032235+josecsfilho@users.noreply.github.com>
2023-08-09 19:54:21 -0300

    Implantando sistema de login na API.

--- api.py
+++ api.py
@@ -111,6 +111,17 @@ def login():
 
     user = User.query.filter_by(name=auth.username).first()
 
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password)
+        token = jwt.encode({'public_id': user.public_id, 'exp': datatime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+
 if __name__ == '__main__':
     with app.app_context():
         # Criar as tabelas no banco de dados
